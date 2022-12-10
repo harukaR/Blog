60bc25861f1634dc710cec7487070e384c2be14d
harukaR <118460418+harukaR@users.noreply.github.com>
2022-12-11 02:38:33 +0900

    カテゴリの別記事一覧の機能を追加

---
+++ src/pages/category/[id].jsx
@@ -0,0 +1,42 @@
+// pages/category/[id].js
+import Link from "next/link";
+import { client } from '../../lib/client'
+
+export default function CategoryId({ blog }) {
+  // カテゴリーに紐付いたコンテンツがない場合に表示
+  if (blog.length === 0) {
+    return <div>ブログコンテンツがありません</div>;
+  }
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              {blog.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "categories" });
+
+  const paths = data.contents.map((content) => `/category/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blogs", queries: { filters: `category[equals]${id}` } });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
